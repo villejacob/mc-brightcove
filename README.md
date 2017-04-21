84fd044944a3954a78ec92dde9492bbb7f26ead1
a-axton <dizzysfarm@gmail.com>
2017-04-21 13:56:30 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+### Rollup UMD Starter
+A super simple starter project for creating rollup UMD's.
+#### Installation
+```
+npm install
+```
+Change "moduleName" in package.json to desired library name.
+#### Usage
+Dev
+```
+npm start
+```
+Fix ESLINT Errors
+```
+npm run fix
+```
+Build dist
+```
+npm run build
+```
---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+import eslint from 'rollup-plugin-eslint';
+import { minify } from 'uglify-js';
+
+const isProd = process.env.PROD;
+const moduleName = require('./package.json').config.moduleName;
+const plugins = [
+  eslint(),
+  json(),
+  babel({ exclude: 'node_modules/**' }),
+  nodeResolve({ jsnext: true, main: true }),
+  commonjs()
+];
+
+if (isProd) {
+  plugins.push(uglify({}, minify));
+}
+
+export default {
+  entry: 'lib/main.js',
+  format: 'amd',
+  moduleName,
+  plugins,
+  dest: 'dist/McBrightcove.js',
+  sourceMap: !isProd
+};
